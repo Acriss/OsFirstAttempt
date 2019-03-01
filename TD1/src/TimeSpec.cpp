4ca846b24795f4f7014a3d10ccaf304f8af67b75
Pierre Mevel <pierre.mevel@student.ecp.fr>
2019-03-01 15:39:10 +0100

    change makefile compiler

--- TD1/src/TimeSpec.cpp
+++ TD1/src/TimeSpec.cpp
@@ -1,8 +1,8 @@
 #include "TimeSpec.hpp"
 
 double timespec_to_ms(const timespec& time_ts) {
-    double seconds = time_ts.tv_sec;
-    double nano_seconds = time_ts.tv_nsec;
+    time_t seconds = time_ts.tv_sec;
+    long nano_seconds = time_ts.tv_nsec;
     return((seconds * 1000) + (nano_seconds/1000000.0));
 }
 
@@ -13,7 +13,7 @@ timespec timespec_from_ms(double time_ms) {
     // amount of milli seconds left after whole seconds taken out
     double milli_seconds_left = time_ms - (timer.tv_sec * 1000);
 
-    if (timer.tv_sec < 0.0) {
+    if (timer.tv_sec < 0) {
         timer.tv_sec += 1;
         timer.tv_nsec = 10e9 - milli_seconds_left * 10e6;
     } else {
@@ -30,14 +30,38 @@ timespec timespec_now() {
 
 timespec timespec_negate(const timespec& time_ts) {
     struct timespec timer;
-    timer.tv_sec = - timer.tv_sec - 1;
-    timer.tv_nsec = timer.tv_nsec == 0 ? 0 : 10e9 - timer.tv_nsec;
+    if (time_ts.tv_nsec != 0) {
+        timer.tv_sec = - time_ts.tv_sec -1;
+        timer.tv_nsec = - (10e9 - time_ts.tv_nsec);
+    
+    } else {
+        timer.tv_nsec = 0;
+        timer.tv_sec = -timer.tv_sec;
+    }
+    
     return timer;
 }
 
+timespec timespec_add(const timespec &time1_ts, const timespec &time2_ts) {
+    struct timespec timer;
+    timer.tv_nsec = time1_ts.tv_nsec + time2_ts.tv_nsec;
+    timer.tv_sec = time1_ts.tv_sec + time2_ts.tv_sec;
+    if (timer.tv_nsec > 10e9) {
+        timer.tv_nsec -= 10e9;
+        timer.tv_sec += 1;
+    }
+    return timer;
+}
 
-
-/* 
-Writing tests
- */
-
+timespec timespec_subtract(const timespec &time1_ts, const timespec &time2_ts) {
+    struct timespec timer;
+    double ns = time1_ts.tv_nsec - time2_ts.tv_nsec;
+    timer.tv_sec = time1_ts.tv_sec - time2_ts.tv_sec;
+    if (ns < 0) {
+        timer.tv_nsec = - ns + 10e9;
+        timer.tv_sec -= 1;
+    } else {
+        timer.tv_nsec = ns;
+    }
+    return timer;
+}
--- TD1/tests/Tests_TimeSpec.cpp
+++ TD1/tests/Tests_TimeSpec.cpp
@@ -1,8 +1,93 @@
 #include "Tests_TimeSpec.hpp"
 
+// timespec_to_ms
 TEST(TimeSpecToMS, NullTimeSpec) {
     timespec time;
     time.tv_nsec = 0;
     time.tv_sec = 0;
     EXPECT_EQ(timespec_to_ms(time), 0);
 }
+TEST(TimeSpecToMS, AllPositiveTimeSpec) {
+    timespec time;
+    time.tv_sec = 1;
+    time.tv_nsec = 900000;
+    EXPECT_EQ(timespec_to_ms(time), 1000.9);
+}
+TEST(TimeSpecToMS, NegativeNSTimespec) {
+    timespec time;
+    time.tv_sec = -1;
+    time.tv_nsec = 900000;
+    EXPECT_EQ(timespec_to_ms(time), -0.1);
+}
+
+// timespec_from_ms
+TEST(TimeSpecFromMS, NullTimeSpec) {
+    timespec time;
+    time.tv_nsec = 0;
+    time.tv_sec = 0;
+    EXPECT_EQ(timespec_from_ms(0), time);
+}
+TEST(TimeSpecFromMS, AllPositiveTimeSpec) {
+    timespec time;
+    time.tv_sec = 1;
+    time.tv_nsec = 900000;
+    EXPECT_EQ(timespec_from_ms(1000.9), time);
+}
+TEST(TimeSpecFromMS, NegativeNSTimespec) {
+    timespec time;
+    time.tv_sec = -1;
+    time.tv_nsec = 900000;
+    EXPECT_EQ(timespec_from_ms(-0.1), time);
+}
+
+// test timespec_negate
+TEST(TimeSpecNegate, NullTimeSpec) {
+    timespec time, compared;
+    time.tv_sec = 0;
+    time.tv_nsec = 0;
+    compared.tv_sec = 0;
+    compared.tv_nsec = 0;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+TEST(TimeSpecNegate, AllPositiveTimeSpec) {
+    timespec time, compared;
+    time.tv_sec = 1;
+    time.tv_nsec = 10e8;
+    compared.tv_sec = -2;
+    compared.tv_nsec = 9*10e8;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+TEST(TimeSpecNegate, NullS_PositiveNS_TimeSpec) {
+    timespec time, compared;
+    time.tv_sec = 0;
+    time.tv_nsec = 10e8;
+    compared.tv_sec = -1;
+    compared.tv_nsec = 9*10e8;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+TEST(TimeSpecNegate, PositiveS_NullNS_TimeSpec) {
+    timespec time, compared;
+    time.tv_sec = 1;
+    time.tv_nsec = 0;
+    compared.tv_sec = -1;
+    compared.tv_nsec = 0;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+TEST(TimeSpecNegate, NegativeS_NullNS_TimeSpec) {
+    timespec time, compared;
+    time.tv_sec = -1;
+    time.tv_nsec = 0;
+    compared.tv_sec = 1;
+    compared.tv_nsec = 0;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+TEST(TimeSpecNegate, NegativeS_PositiveNS_TimeSpec) {
+    timespec time, compared;
+    time.tv_sec = -1;
+    time.tv_nsec = 10e8;
+    compared.tv_sec = 0;
+    compared.tv_nsec = 9*10e8;
+    EXPECT_EQ(compared, timespec_negate(time));
+}
+
+// Test timespec_add
